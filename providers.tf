8eade5acaf6fa083e95f4f9397fd56db857e18cf
Philip Almueti <philip.almueti@ucop.edu>
2024-08-30 15:24:41 -0700

    Set default aws version that supports 2nd-gen fsx ontap

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.53"
+      version = "~> 5.58" # Earliest version that supports fsx-2nd-gen
     }
     random = {
       source = "hashicorp/random"
